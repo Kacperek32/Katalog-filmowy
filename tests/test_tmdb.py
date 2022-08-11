7a3c4ee45c50da5131843c2bd4f2002ed3aae177
Kacper Manterys <kacpermanterys@gmail.com>
2022-08-11 17:09:45 +0200

    Add testing v2

--- tests/test_tmdb.py
+++ tests/test_tmdb.py
@@ -1,5 +1,7 @@
 import tmdb_client
 from unittest.mock import Mock
+import pytest
+from main import app
 
 def test_get_movies_list_type_popular():
    movies_list = tmdb_client.get_movies_list(list_type="popular")
@@ -19,3 +21,15 @@ def test_get_single_movie_cast(monkeypatch):
 
    movies_list = tmdb_client.get_single_movie_cast(movie_id="")
    assert movies_list == mock_movies_list
+
+@pytest.mark.parametrize('name, code',(
+    ('/', 200),
+    ('/popular/', 200),
+))
+
+def test_homepage(name, code, monkeypatch):
+    api_mock = Mock(return_value={'results': []})
+    monkeypatch.setattr("main.popular", api_mock)
+    with app.test_client() as client:
+       response = client.get('/')
+       assert response.status_code == 200
